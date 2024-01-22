5f356128dac1f8eaf655920c38145ca68607e23e
junhochoi-dev <cjh970422@gmail.com>
2024-01-22 18:31:08 +0900

    Feat: Kafka config

---
+++ src/main/java/com/project/chatserver/config/KafkaConfig.java
@@ -0,0 +1,12 @@
+package com.project.chatserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+
+@EnableKafka
+@Configuration
+public class KafkaConfig {
+    // Producer
+    // Consumer
+}
